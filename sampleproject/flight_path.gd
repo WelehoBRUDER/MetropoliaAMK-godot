a26117c58cbe07530f9103b726a9e25837f80610
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-04-04 11:59:23 +0300

    birb moves

--- sampleproject/flight_path.gd
+++ sampleproject/flight_path.gd
@@ -3,4 +3,6 @@ extends PathFollow2D
 @export var flight_speed = 100
 
 func _physics_process(delta: float) -> void:
-	pass
+	var oldPos = global_position
+	progress += flight_speed * delta
+	$Bird.flip_h = global_position.x < oldPos.x
